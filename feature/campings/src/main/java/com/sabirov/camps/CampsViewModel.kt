6848fa0e32726158630bf9c5c75ac8eac7cfd953
NSabirov <nikolay.sabirov@tattelecom.digital>
2023-09-22 08:42:46 +0300

    Добавление core модуля и базы данных

--- feature/campings/src/main/java/com/sabirov/camps/CampsViewModel.kt
+++ feature/campings/src/main/java/com/sabirov/camps/CampsViewModel.kt
@@ -1,21 +1,12 @@
 package com.sabirov.camps
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.core_api.dto.Camping
 import com.sabirov.CampingsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
